29d3a047e39e82f4ebb97780522462a9ee59fd0e
aipnur <aipnurhayadi08@gmail.com>
2021-06-17 20:14:59 +0700

    ubah title

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'aipnurhayadi.github.io',
+    title: 'Aipnur',
     htmlAttrs: {
       lang: 'en'
     },
